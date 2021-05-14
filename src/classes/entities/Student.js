3a538fad0e3ee4c91173edfaf66d9b7596808cae
moonstar-x <christian.lopez99@outlook.com>
2021-05-14 17:41:18 -0500

    Created Student.Partial class to handle updates.

--- src/classes/entities/Student.js
+++ src/classes/entities/Student.js
@@ -11,25 +11,36 @@ class Student {
     this.id = uuid();
   }
 
-  toString() {
-    return JSON.stringify(this.data);
-  }
-
   static from(obj, toCreate = true) {
-    const validated = Validator.validate(obj, toCreate ? Student.SCHEMA.CREATE : Student.SCHEMA.UPDATE);
+    if (toCreate) {
+      const validated = Validator.validate(obj, Student.SCHEMA.CREATE);
+      return new Student(validated);
+    }
 
-    return new Student(validated);
+    const validated = Validator.validate(obj, Student.SCHEMA.UPDATE);
+    return new Student.Partial(validated);
   }
 }
 
 Student.SCHEMA = {
   CREATE: Joi.object({
     id: Joi.forbidden(),
-    name: Joi.string().trim().required()
+    name: Joi.string().trim().required(),
+    lastName: Joi.string().trim().required()
   }),
   UPDATE: Joi.object({
-    id: Joi.forbidden()
+    id: Joi.forbidden(),
+    name: Joi.string().trim(),
+    lastName: Joi.string().trim()
   })
+};
+
+Student.Partial = class Partial {
+  constructor(validated) {
+    for (const key in validated) {
+      this[key] = validated[key];
+    }
+  }
 };
 
 module.exports = Student;
