8713435876f2f965a7f1122427744456724cd9dc
moonstar-x <christian.lopez99@outlook.com>
2021-05-15 09:51:37 -0500

    Created clubRouter to handle club routes.

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const studentRouter = require('./studentRouter');
+const clubRouter = require('./clubRouter');
 
 const api = express.Router();
 
 api.use('/', studentRouter);
+api.use('/', clubRouter);
 
 module.exports = api;
