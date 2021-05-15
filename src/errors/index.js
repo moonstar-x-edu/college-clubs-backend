bf4499fad227a81e1d2eda1e3fdf0645fd4417aa
moonstar-x <christian.lopez99@outlook.com>
2021-05-15 13:07:40 -0500

    Created DatabaseError class.

--- src/errors/index.js
+++ src/errors/index.js
@@ -3,11 +3,13 @@ const InvalidBodyError = require('./InvalidBodyError');
 const InvalidObjectTypeError = require('./InvalidObjectTypeError');
 const ResourceNotFoundError = require('./ResourceNotFoundError');
 const ResourceAlreadyExistsError = require('./ResourceAlreadyExistsError');
+const DatabaseError = require('./DatabaseError');
 
 module.exports = {
   InvalidStatusError,
   InvalidBodyError,
   InvalidObjectTypeError,
   ResourceNotFoundError,
-  ResourceAlreadyExistsError
+  ResourceAlreadyExistsError,
+  DatabaseError
 };
