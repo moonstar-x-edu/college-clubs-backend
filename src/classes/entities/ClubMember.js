d75d72ea0d34dbc2fd2f21bcbd09147dd8d83b85
moonstar-x <christian.lopez99@outlook.com>
2021-05-15 12:32:28 -0500

    Created ClubMember entity.

---
+++ src/classes/entities/ClubMember.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const { v4: uuid } = require('uuid');
+const Validator = require('./Validator');
+
+class ClubMember {
+  constructor(validated) {
+    for (const key in validated) {
+      this[key] = validated[key];
+    }
+
+    this.id = uuid();
+  }
+
+  static from(obj, toCreate = true) {
+    if (toCreate) {
+      const validated = Validator.validate(obj, ClubMember.SCHEMA.CREATE);
+      return new ClubMember(validated);
+    }
+
+    const validated = Validator.validate(obj, ClubMember.SCHEMA.UPDATE);
+    return new ClubMember.Partial(validated);
+  }
+}
+
+ClubMember.SCHEMA = {
+  CREATE: Joi.object({
+    id: Joi.forbidden(),
+    nickname: Joi.string().trim().required(),
+    imageURL: Joi.string().uri().allow(null).allow('').default(null),
+    joinedAt: Joi.string().isoDate().required()
+  }),
+  UPDATE: Joi.object({
+    id: Joi.forbidden(),
+    nickname: Joi.string().trim(),
+    imageURL: Joi.string().uri().allow(null).allow(''),
+    joinedAt: Joi.forbidden()
+  })
+};
+
+ClubMember.Partial = class Partial {
+  constructor(validated) {
+    for (const key in validated) {
+      this[key] = validated[key];
+    }
+  }
+};
+
+module.exports = ClubMember;
